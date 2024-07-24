ece20c627a2d48a78f0a4260568d2cc993e804c4
Yi Yao <leaf_yao8621@hotmail.com>
2024-07-23 17:25:28 -0700

    Updates

--- fsi_lite/src/parser/handler/handler_dot.c
+++ fsi_lite/src/parser/handler/handler_dot.c
@@ -8,12 +8,14 @@ int parser_handler_dot(void) {
     } else {
         if (parser_state & PARSER_STATE_INTERPRET) {
             if (vm_interpreted_cur == vm_interpreted_end) {
+                parser_status = PARSER_STATUS_END;
                 return PARSER_STATUS_INTERPRETED_OVERFLOW;
             }
             *(vm_interpreted_cur++) = VM_INSTRUCTION_PINT;
         }
         if (parser_state & PARSER_STATE_COMPILE) {
             if (vm_compiled_cur == vm_interpreted_end) {
+                parser_status = PARSER_STATUS_END;
                 return PARSER_STATUS_COMPILED_OVERFLOW;
             }
             *(vm_compiled_cur++) = VM_INSTRUCTION_PINT;
