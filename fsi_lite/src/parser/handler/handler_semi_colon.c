4c7d345c93a78d651e7541e31954515675e84e07
Yi Yao <leaf_yao8621@hotmail.com>
2024-08-01 16:23:12 -0700

    Updates

--- fsi_lite/src/parser/handler/handler_semi_colon.c
+++ fsi_lite/src/parser/handler/handler_semi_colon.c
@@ -4,7 +4,14 @@
 
 int parser_handler_semi_colon(void) {
     if (parser_state & PARSER_STATE_INTERPRET) {
+        parser_status = PARSER_STATUS_END;
         return PARSER_STATUS_NOT_IN_COMPILATION_MODE;
+    }
+    if (
+        parser_conditional_stack_cur != parser_conditional_stack ||
+        parser_loop_stack_cur != parser_loop_stack) {
+        parser_status = PARSER_STATUS_END;
+        return PARSER_STATUS_PARSER_CONTROL_STACK_MISMATCH;
     }
     if (vm_compiled_cur == vm_compiled_end) {
         parser_status = PARSER_STATUS_END;
