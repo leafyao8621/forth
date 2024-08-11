1f0060395f99689d92514784c8c98fafefb48fc6
Yi Yao <leaf_yao8621@hotmail.com>
2024-08-11 15:16:25 -0700

    Updates

--- fsi_lite/src/parser/handler/handler_begin.c
+++ fsi_lite/src/parser/handler/handler_begin.c
@@ -2,21 +2,17 @@
 #include "../../vm/vm.h"
 #include "../../util/status.h"
 
-int parser_handler_do(void) {
+int parser_handler_begin(void) {
     if (parser_state & PARSER_STATE_INTERPRET) {
         parser_status = PARSER_STATUS_END;
         return PARSER_STATUS_NOT_IN_COMPILATION_MODE;
-    }
-    if (vm_compiled_cur == vm_compiled_end) {
-        parser_status = PARSER_STATUS_END;
-        return PARSER_STATUS_COMPILED_OVERFLOW;
     }
     *(vm_compiled_cur++) = VM_INSTRUCTION_2PUSHC;
     if (parser_loop_stack_cur == parser_loop_stack_end) {
         parser_status = PARSER_STATUS_END;
         return PARSER_STATUS_PARSER_CONTROL_STACK_OVERFLOW;
     }
-    *(parser_loop_stack_cur++) = PARSER_CONTROL_DO;
+    *(parser_loop_stack_cur++) = PARSER_CONTROL_BEGIN;
     *(uint8_t**)parser_loop_stack_cur = vm_compiled_cur;
     parser_loop_stack_cur += sizeof(uintptr_t);
     return PARSER_STATUS_OK;
