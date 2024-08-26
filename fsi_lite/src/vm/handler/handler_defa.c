78be2404ffc1ac35aaafa2368ac0e579bc214fea
Yi Yao <leaf_yao8621@hotmail.com>
2024-08-26 15:17:49 -0700

    Updates

--- fsi_lite/src/vm/handler/handler_defa.c
+++ fsi_lite/src/vm/handler/handler_defa.c
@@ -9,5 +9,6 @@ int vm_handler_defa(void) {
         vm_state = VM_STATE_HALTED;
         return VM_STATUS_INVALID_MEMORY_ALLOCATION;
     }
+    *(uint8_t**)vm_memory_cur += sizeof(uintptr_t);
     return VM_STATUS_OK;
 }
