c0b11158e7d405a7e190004e8ba4501ede96f20f
Yi Yao <leaf_yao8621@hotmail.com>
2024-10-09 15:12:45 -0700

    Updates

--- fsi/script/a.fs
+++ fsi/script/a.fs
@@ -1,2 +1,15 @@
-: cr create 1 cells allot 65 emit 10 emit ;
-: va variable 66 emit 10 emit ;
+: point create 2 cells allot ;
+: point-x ;
+: point-y 1 cells + ;
+: point-print dup point-x @ . 32 emit point-y @ . cr ;
+
+point pa
+10 pa point-x !
+20 pa point-y !
+
+point pb
+30 pb point-x !
+40 pb point-y !
+
+pa point-print
+pb point-print
