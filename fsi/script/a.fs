6716ce7c5d99a626287fdfa7b180b89aa47ba471
Yi Yao <leaf_yao8621@hotmail.com>
2025-04-17 12:15:08 -0700

    Updates

--- fsi/script/a.fs
+++ fsi/script/a.fs
@@ -1,13 +1,2 @@
-create arr char A c, char T c, char G c, char C c,
-
-: one
-    0 do
-        r> dup . cr >r
-    loop
-;
-
-: two
-    0 do
-        2r> 2dup . space . cr 2>r
-    loop
-;
+2 chars . cr
+1 char+ . cr
