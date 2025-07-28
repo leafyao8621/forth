cd5d8abdfea498e329dd176637202730cdcacd0f
Yi Yao <leaf_yao8621@hotmail.com>
2025-07-28 14:40:42 -0700

    Updates

--- fsi/script/a.fs
+++ fsi/script/a.fs
@@ -1,6 +1,16 @@
 : init ." Before load" cr ;
 init
 mload" test"
-hello
-hello.1
-hello.2
+: main
+    ." Hello 1" cr
+    hello
+    ." Hello P 1" cr
+    hello.p
+    ." Hello R 1" cr
+    hello.r
+    ." Hello 1 1" cr
+    hello.1
+    ." Hello 2 1" cr
+    hello.2
+;
+main
