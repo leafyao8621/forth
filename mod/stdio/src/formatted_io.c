3dab93cbfaf9db1f1f593c2b23f2f11bf7e0ffdb
Yi Yao <leaf_yao8621@hotmail.com>
2025-07-31 14:22:14 -0700

    Updates

--- mod/stdio/src/formatted_io.c
+++ mod/stdio/src/formatted_io.c
@@ -31,3 +31,36 @@ int vm_ext_printf_dot_f(ForthVM *vm) {
     );
     return VM_STATUS_OK;
 }
+
+int vm_ext_fprintf_dot_d(ForthVM *vm) {
+    if (vm->data_stack_cur - sizeof(uintptr_t) * 3 < vm->data_stack) {
+        vm->state = VM_STATE_HALTED;
+        return VM_STATUS_DATA_STACK_UNDERFLOW;
+    }
+    vm->data_stack_cur -= sizeof(uintptr_t) * 3;
+    fprintf(
+        *(FILE**)vm->data_stack_cur,
+        *(char**)(vm->data_stack_cur + sizeof(uintptr_t)),
+        *(uintptr_t*)(vm->data_stack_cur + (sizeof(uintptr_t) << 1))
+    );
+    return VM_STATUS_OK;
+}
+
+int vm_ext_fprintf_dot_f(ForthVM *vm) {
+    if (vm->data_stack_cur - (sizeof(uintptr_t) << 1) < vm->data_stack) {
+        vm->state = VM_STATE_HALTED;
+        return VM_STATUS_DATA_STACK_UNDERFLOW;
+    }
+    if (vm->float_stack_cur == vm->float_stack) {
+        vm->state = VM_STATE_HALTED;
+        return VM_STATUS_FLOAT_STACK_UNDERFLOW;
+    }
+    vm->data_stack_cur -= sizeof(uintptr_t) << 1;
+    vm->float_stack_cur -= sizeof(double);
+    fprintf(
+        *(FILE**)vm->data_stack_cur,
+        *(char**)(vm->data_stack_cur + sizeof(uintptr_t)),
+        *(double*)vm->float_stack_cur
+    );
+    return VM_STATUS_OK;
+}
--- mod/stdio/test/formatted_io.fs
+++ mod/stdio/test/formatted_io.fs
@@ -1,9 +1,18 @@
 mload" stdio"
 
 : main
-    z" %s" z" abcd" puts
     z" 0x%016lx" 65535 printf.d
     cr
     z" %010.2lf" 65535.456 d>f printf.f
+    cr
+    z" dump" z" w" fopen
+    dup
+    z" 0x%016lx" 65535 fprintf.d
+    dup
+    z" %010.2lf" 65535.456 d>f fprintf.f
+    fclose
+    stdout z" 0x%016lx" 65535 fprintf.d
+    cr
+    stdout z" %010.2lf" 65535.456 d>f fprintf.f
     cr
 ;
